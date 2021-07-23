e0e08897479179d4693b259adb628206a1d83962
rafabolivar <37837380+rafabolivar@users.noreply.github.com>
2021-07-23 16:16:49 +0200

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
     $cons_usuario="root";
     $cons_contra="nutanix/4u";
     $cons_base_datos="mysql";
-    $cons_equipo="10.55.82.59";
+    $cons_equipo="10.55.77.51";
     
     $obj_conexion = 
     mysqli_connect($cons_equipo,$cons_usuario,$cons_contra,$cons_base_datos);
