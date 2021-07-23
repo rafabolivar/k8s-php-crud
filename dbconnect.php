5272832653163fd3f3f16bfab037a037d874e517
rafabolivar <37837380+rafabolivar@users.noreply.github.com>
2021-07-23 16:49:04 +0200

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -4,8 +4,7 @@
     $cons_base_datos="mysql";
     $cons_equipo="10.55.77.51";
     
-    $obj_conexion = 
-    mysqli_connect($cons_equipo,$cons_usuario,$cons_contra,$cons_base_datos);
+    $obj_conexion = mysqli_connect($cons_equipo,$cons_usuario,$cons_contra,$cons_base_datos);
     if(!$obj_conexion)
     {
         echo "<h3>No se ha podido conectar PHP - MySQL, verifique sus datos.</h3><hr><br>";
