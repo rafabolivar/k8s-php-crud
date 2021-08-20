05bb8d4e229bfa58210ba9c547908ad2cf607c94
rafabolivar <37837380+rafabolivar@users.noreply.github.com>
2021-08-20 03:43:27 +0200

    Update 1-Metallb.sh

--- scripts/1-Metallb.sh
+++ scripts/1-Metallb.sh
@@ -14,7 +14,8 @@ sleep 5
 NETROOT=`kubectl describe nodes | grep Internal | head -1 | awk {'print$2'} | awk -F. {'print$1"."$2"."$3'}`
 IP_BEGIN=`echo "$NETROOT.$OFFSET_BEGIN"`
 IP_END=`echo "$NETROOT.$OFFSET_END"`
-cp /Users/rafael.bolivar/Documents/Entorno_POC_Rafa/Nginx_Metallb/metallb-config-orig.yaml /Users/rafael.bolivar/Documents/Entorno_POC_Rafa/Nginx_Metallb/metallb-config.yaml
-sed -i '' -e "s/IP_BEGIN/$IP_BEGIN/" /Users/rafael.bolivar/Documents/Entorno_POC_Rafa/Nginx_Metallb/metallb-config.yaml
-sed -i '' -e "s/IP_END/$IP_END/" /Users/rafael.bolivar/Documents/Entorno_POC_Rafa/Nginx_Metallb/metallb-config.yaml
-kubectl apply -f /Users/rafael.bolivar/Documents/Entorno_POC_Rafa/Nginx_Metallb/metallb-config.yaml
+
+cp /home/ubuntu/demo/scripts/metallb-config-orig.yaml /home/ubuntu/demo/scripts/metallb-config.yaml
+sed -i '' -e "s/IP_BEGIN/$IP_BEGIN/" /home/ubuntu/demo/scripts/metallb-config.yaml
+sed -i '' -e "s/IP_END/$IP_END/" /home/ubuntu/demo/scripts/metallb-config.yaml
+kubectl apply -f /home/ubuntu/demo/scripts/metallb-config.yaml
