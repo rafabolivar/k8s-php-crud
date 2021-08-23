3fa38ec8f948b98a479827da90399ca7a1c952a8
root <root@mgmtnode.ntnxlab.local>
2021-08-23 21:35:58 +0000

    test

--- app/config.php
+++ app/config.php
@@ -1,7 +1,7 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', '10.55.58.59');
+define('DB_SERVER', '10.38.58.59');
 define('DB_USERNAME', 'nutanix');
 define('DB_PASSWORD', 'nutanix/4u');
 define('DB_NAME', 'mydb1');
